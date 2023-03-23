646fd6fd3e4c7c009ad37ef1c0343098972d1910
sleepy <244546531@qq.com>
2023-03-23 22:59:49 +0800

    add or delete one song,then print the list

--- SongList.py
+++ SongList.py
@@ -1,3 +1,21 @@
+def print_list(list):
+    for song in list:
+        print(f"I like {song}")
+
+
 favourite_songs = ["7 rings", "SOS", "杀手"]
-for song in favourite_songs:
-    print(f"I like {song}")
+
+# print song in the list
+print_list(favourite_songs)
+
+# delete one song
+delete_song = favourite_songs.pop()
+print(f"delete {delete_song},the list is:")
+print_list(favourite_songs)
+
+# add one song
+add_song = "半岛铁盒"
+favourite_songs.append(add_song)
+print(f"add {add_song},the list is:")
+print_list(favourite_songs)
+
